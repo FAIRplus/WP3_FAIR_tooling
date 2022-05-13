8395d88ad89756db42459905c7080137c1dce075
Eva Martin del Pico <eva.mdpico@gmail.com>
2022-05-13 14:25:42 +0200

    Zooma search avoided if edam terms come from autocomplete

--- backend/biotools_API_querying.py
+++ backend/biotools_API_querying.py
@@ -30,10 +30,10 @@ class tools_discoverer(object):
             prompt_text = f"Loading input files..."
             print(f"{bcolors.OKBLUE}{prompt_text}{bcolors.ENDC}")    
 
-        self.terms_input = kw_w # list(dict('keyword', 'weight'))
+        self.terms_input = kw_w # list(dict('keyword', 'ClassId','weight'))
         print(self.terms_input)
         # keywords in:
-        self.keywords_weights = pd.DataFrame(kw_w)
+        self.keywords_weights = []
         # zooma terms in:
         self.edam_terms = []
         # terms stored here
@@ -71,31 +71,44 @@ class tools_discoverer(object):
         '''
         if self.verbosity:
             print("Looking up terms in ZOOMA")
-        for keyword in list(self.keywords_weights['keyword']):
-            if self.verbosity:
-                print(f"{bcolors.BOLD}{keyword}{bcolors.ENDC}")
-            confident_matches = za.zooma_single_lookup(keyword)
-            if confident_matches:
-                if self.verbosity:
-                    print(f"Matches found in EDAM:")
-                    [print(f"{match['label']} - {match['confidence']} - {match['edam_term']}") for match in confident_matches]
+
+        for term in self.terms_input:
+            keyword = term['keyword']
+            # If edam term, directly add to edam list
+            print(keyword)
+            if term['classId']:
+                self.edam_terms.append(keyword)
+                term['weight']= term['weight'] + 1
+                self.keywords_weights.append(term)
                 
-                w = self.keywords_weights.loc[self.keywords_weights['keyword']==keyword]['weight'].values[0]
-                for match in confident_matches:
-                    term = match['edam_term'].strip('\n')
-                    self.edam_terms.append(term)
-                    #self.terms_label[match] = str(match['label']).strip('\n')
-                    self.keywords_weights = self.keywords_weights.append({'keyword':term, 'weight':w+1}, ignore_index=True)
-                self.free_terms.append(keyword)
             else:
-                self.free_terms.append(keyword)
+                if self.verbosity:
+                    print(f"{bcolors.BOLD}{keyword}{bcolors.ENDC}")
+                confident_matches = za.zooma_single_lookup(keyword)
+                w = term['weight']
+                if confident_matches:
+                    if self.verbosity:
+                        print(f"Matches found in EDAM:")
+                        [print(f"{match['label']} - {match['confidence']} - {match['edam_term']}") for match in confident_matches]
+                    
+                    for match in confident_matches:
+                        term_label = match['edam_term'].strip('\n')
+                        self.edam_terms.append(term_label)
+                        #self.terms_label[match] = str(match['label']).strip('\n')
+                        self.keywords_weights.append({'keyword':term_label, 'classId':None, 'weight':w+1})
+                    self.free_terms.append(keyword)
+                else:
+                    self.keywords_weights.append({'keyword':keyword, 'classId':None, 'weight':w})
+                    self.free_terms.append(keyword)
+
+        self.keywords_weights = pd.DataFrame(self.keywords_weights)
         print(self.keywords_weights)
         print('Zooma lookup done')
 
 
     def query_terms(self):
         print('edam terms: ' + str(self.edam_terms))
-        print('free terms' + str(self.free_terms))
+        print('free terms: ' + str(self.free_terms))
         query = db_retrieval.query(self.edam_terms, self.free_terms)
         query.getData() # perform db search
         self.results = query.results
